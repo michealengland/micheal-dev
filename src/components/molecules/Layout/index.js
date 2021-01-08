fec17ff54dd1e7561aa66a7847f7d683510b9081
Mike England <mike.england@webdevstudios.com>
2021-01-08 09:01:40 -0500

    Add background color to layout component

--- src/components/molecules/Layout/index.js
+++ src/components/molecules/Layout/index.js
@@ -1,9 +1,10 @@
 import PropTypes from 'prop-types'
 import React from 'react'
+import styles from './Layout.module.css'
 
-const Layout = ({ children }) => (
-  <div id="layout">{children}</div>
-)
+const Layout = ({ children }) => {
+  return <div id="layout" className={styles.layout}>{children}</div>
+}
 
 export default Layout
 
