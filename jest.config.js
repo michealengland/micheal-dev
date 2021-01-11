7bea70778fa4828e79b2a96a4bda1630ce8c30ca
Mike England <mike.england@webdevstudios.com>
2021-01-10 22:33:27 -0500

    Fix alias for jest
    Setup moduleNameMapper for aliases so that tests can run.
    
    See https://jestjs.io/docs/en/configuration#modulenamemapper-objectstring-string--arraystring

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,4 @@
+
 module.exports = {
   transform: {
     '^.+\\.jsx?$': '<rootDir>/jest-preprocess.js',
@@ -5,6 +6,9 @@ module.exports = {
   moduleNameMapper: {
     '.+\\.(css|styl|less|sass|scss)$': 'identity-obj-proxy',
     '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/file-mock.js',
+    '^@components(.*)$': '<rootDir>/src/components$1',
+    '^@utilities(.*)$': '<rootDir>/src/utilities$1',
+    '^@static(.*)$': '<rootDir>/src/static$1',
   },
   testPathIgnorePatterns: ['node_modules', '\\.cache', '<rootDir>.*/public'],
   transformIgnorePatterns: ['node_modules/(?!(gatsby)/)'],
