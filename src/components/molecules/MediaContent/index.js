d33c515b9e2acdfbf9753e955f714e91bd8253b7
Mike England <mike.england@webdevstudios.com>
2021-01-12 23:13:11 -0500

    Add classNames to MediaContent

--- src/components/molecules/MediaContent/index.js
+++ src/components/molecules/MediaContent/index.js
@@ -2,18 +2,19 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Heading from '@components/atoms/Heading'
 import createMarkup from '@utilities/createMarkup'
+import styles from './MediaContent.module.css'
 
 export default function MediaContent({content, img, imgAlt, title, titleTag}) {
   return (
-    <section>
+    <section className={styles.mediaContent}>
       {(title || content) &&
-        <div>
+        <div className={styles.content}>
           {title && <Heading tag={titleTag}>{title}</Heading>}
           {content && <div dangerouslySetInnerHTML={ createMarkup(content) } />}
         </div>
       }
       {img &&
-        <figure>
+        <figure className={styles.media}>
           <img src={img} alt={imgAlt} />
         </figure>
       }
