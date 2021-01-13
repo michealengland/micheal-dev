708d4dc24e475d592a65eccb4402d56876238f5a
Mike England <mike.england@webdevstudios.com>
2021-01-12 23:06:37 -0500

    Add MediaContent example from original site

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import * as React from 'react'
 import Layout from '@components/molecules/Layout'
 import Hero from '@components/molecules/Hero'
 import Content from '@components/molecules/Content'
+import MediaContent from '@components/molecules/MediaContent'
 
 const IndexPage = () => {
   return (
@@ -15,6 +16,23 @@ const IndexPage = () => {
         title={ 'About Me' }
         titleTag="h2"
         content={ 'I\'m a Frontend Engineer at WebDevStudios and I\'m passionate about all things JavaScript. Building awesome websites, tools, and web applications is my passion!' }
+      />
+      <MediaContent
+        // backgroundColor={ `#f2e6f0` }
+        // imgClip={ true }
+        img="/images/farzad-nazifi-71686-unsplash.jpg"
+        title="Code Skills"
+        content={`
+        <p>I enjoy writing clean code and creating user experiences. Here are some of the areas I'm focused on currently.</p>
+        <ul>
+          <li>JavasScript</li>
+          <li>Gutenberg</li>
+          <li>React</li>
+          <li>Webpack</li>
+          <li>Gatsby</li>
+          <li>Jest (WIP)</li>
+        </ul>
+        `}
       />
     </Layout>
   )
