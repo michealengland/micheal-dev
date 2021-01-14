e9d89c51f0705ca0d0609d52d2dbef3739d43e62
Mike England <mike.england@webdevstudios.com>
2021-01-14 08:59:06 -0500

    Create Footer component from original

---
+++ src/components/organisms/Footer/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer>
+      <p>© {new Date().getFullYear()} Mike England, Built with <a href="https://www.gatsbyjs.org">Gatsby</a></p>
+    </footer>
+  )
+}
