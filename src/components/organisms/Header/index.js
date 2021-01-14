2c8aeda9577e7929b2f961484337e594bd9ab544
Mike England <mike.england@webdevstudios.com>
2021-01-14 08:34:13 -0500

    Add style module to Header

--- src/components/organisms/Header/index.js
+++ src/components/organisms/Header/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Link from '@components/atoms/Link'
 import Logo from '@content/assets/me-icon.png'
+import styles from './Header.module.css'
 
 export default function Header() {
-  return <header>
-    <h1>
+  return <header className={styles.header}>
+    <h1 className={styles.logo}>
       <Link to={ '/' }>
         <img src={ Logo } alt="Micheal.dev logo" />
       </Link>
