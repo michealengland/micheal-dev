df6b483a70247c573b72352dafa45d3f10cddc49
Mike England <mike.england@webdevstudios.com>
2021-01-13 23:41:04 -0500

    Update Layout with Header

--- src/components/molecules/Layout/index.js
+++ src/components/molecules/Layout/index.js
@@ -1,9 +1,15 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 import styles from './Layout.module.css'
+import Header from '@components/organisms/Header'
 
 const Layout = ({children}) => {
-  return <div id="layout" className={styles.layout}>{children}</div>
+  return (
+    <div id="layout" className={styles.layout}>
+      <Header />
+      {children}
+    </div>
+  )
 }
 
 export default Layout
