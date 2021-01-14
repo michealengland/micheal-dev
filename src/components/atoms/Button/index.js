b848d07054e75b95cb32e8a0dac227cda62280c6
Mike England <mike.england@webdevstudios.com>
2021-01-13 22:53:09 -0500

    Add className prop to Button component

--- src/components/atoms/Button/index.js
+++ src/components/atoms/Button/index.js
@@ -2,9 +2,10 @@ import React from 'react'
 import Link from '@components/atoms/link'
 import ProptTypes from 'prop-types'
 import styles from './Button.module.css'
+import cn from 'classnames'
 
-const Button = ({label, to}) => (
-  <Link className={styles.button} to={to}>
+const Button = ({className, label, to}) => (
+  <Link className={cn(styles.button, className)} to={to}>
     {label}
   </Link>
 )
@@ -12,6 +13,7 @@ const Button = ({label, to}) => (
 export default Button
 
 Button.propTypes = {
+  className: ProptTypes.string,
   label: ProptTypes.string,
   to: ProptTypes.string,
 }
