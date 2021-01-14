2b1fa7d103c4236e53d7042c9e97bc46bd687dbe
Mike England <mike.england@webdevstudios.com>
2021-01-14 08:59:26 -0500

    Add Footer Layout

--- src/components/molecules/Layout/index.js
+++ src/components/molecules/Layout/index.js
@@ -2,12 +2,14 @@ import PropTypes from 'prop-types'
 import React from 'react'
 import styles from './Layout.module.css'
 import Header from '@components/organisms/Header'
+import Footer from '@components/organisms/Footer'
 
 const Layout = ({children}) => {
   return (
     <div id="layout" className={styles.layout}>
       <Header />
       {children}
+      <Footer />
     </div>
   )
 }
