ad049fde8c6535a7be7b80f18518004f4ea5d2e3
Mike England <mike.england@webdevstudios.com>
2021-01-14 23:55:05 -0500

    Setup blog page based on original
    Due to some issues, this needed refactored and simplified.
    
    - Queries were no longer the same
    - Extra markup was removed
    - Replaced components

---
+++ src/pages/blog.js
@@ -0,0 +1,67 @@
+import React from 'react'
+// import SEO from '../components/seo'
+import Layout from '@components/organisms/Layout'
+import Link from '@components/atoms/Link'
+import Heading from '@components/atoms/Heading'
+import PropTypes from 'prop-types'
+import {graphql} from 'gatsby'
+import createMarkup from '@utilities/createMarkup'
+
+export default function Blog({data}) {
+  const posts = data?.allMdx?.nodes
+
+  return (
+    <Layout>
+      {/* <SEO title="All posts" /> */}
+      <section>
+        <Heading tag="h1">Blog</Heading>
+        {!! posts?.length > 0 && posts.map((post, index) => {
+          const {
+            excerpt,
+            frontmatter:{
+              date,
+              title
+            },
+            slug
+          } = post
+
+          return (
+            <article key={index}>
+              <Heading tag="h2">
+                <Link to={`blog${slug}`}>
+                  {title}
+                </Link>
+              </Heading>
+              <small>{date}</small>
+              <p dangerouslySetInnerHTML={createMarkup(excerpt)} />
+            </article>
+          )
+        })}
+      </section>
+    </Layout>
+  )
+}
+
+Blog.propTypes = {
+  data: PropTypes.shape({
+    allMdx: PropTypes.shape({
+      nodes: PropTypes.arrayOf(PropTypes.object)
+    }),
+  }),
+}
+
+export const pageQuery = graphql`
+query BlogQuery {
+  allMdx {
+    totalCount
+    nodes {
+      excerpt
+      frontmatter {
+        title
+        date(formatString: "MMMM DD, YYYY")
+      }
+      slug
+    }
+  }
+}
+`
