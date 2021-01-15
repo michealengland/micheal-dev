49af95430669f8790ebd77c6b17795b155449edf
Mike England <mike.england@webdevstudios.com>
2021-01-15 00:37:32 -0500

    Add css modules to PostHeader

--- src/components/molecules/PostHeader/index.js
+++ src/components/molecules/PostHeader/index.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Heading from '@components/atoms/Heading'
+import styles from './PostHeader.module.css'
 
 export default function PostHeader({content, title}) {
   return (
-    <header>
-      <Heading tag="h1">{title}</Heading>
-      {content && <p>{content}</p>}
+    <header className={styles.postHeader}>
+      <div className={styles.inner}>
+        <Heading className={styles.title} tag="h1">{title}</Heading>
+        {content && <p className={styles.content}>{content}</p>}
+      </div>
     </header>
   )
 }
