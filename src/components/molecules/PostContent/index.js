470b40b81dfe6ca1dbe4f5784fe984b8b9496fec
Mike England <mike.england@webdevstudios.com>
2021-01-22 16:35:42 -0500

    Update PostContent with layout styles

--- src/components/molecules/PostContent/index.js
+++ src/components/molecules/PostContent/index.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import styles from './PostContent.module.css'
+import cn from 'classnames'
 
 export default function PostContent({children, className}) {
-  return <section className={className}>{children}</section>
+  return (
+    <section className={cn(styles.postContent, className)}>
+      <div className={styles.inner}>{children}</div>
+    </section>
+  )
 }
 
 PostContent.propTypes = {
