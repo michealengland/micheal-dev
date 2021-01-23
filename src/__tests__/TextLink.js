012e6a772f4b723133c409ee6b2a5b94b61aa903
Mike England <mike.england@webdevstudios.com>
2021-01-23 01:05:35 -0500

    Adjust TextLink test
    Add a case for external link and generate new snapshot.

--- src/__tests__/TextLink.js
+++ src/__tests__/TextLink.js
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, {Fragment} from 'react'
 import renderer from 'react-test-renderer'
 import TextLink from '../components/atoms/TextLink'
 describe('TextLink', () => {
   it('renders correctly', () => {
     const tree = renderer
-      .create(<TextLink to="/">Home Link</TextLink>)
+      .create(
+        <Fragment>
+          <TextLink to="/">Home Link</TextLink>
+          <TextLink to="https://micheal.dev">Home Link</TextLink>
+        </Fragment>
+      )
       .toJSON()
     expect(tree).toMatchSnapshot()
   })
