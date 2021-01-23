26590b01beca0b48f031cffefb5580a3ed0eb2b7
Mike England <mike.england@webdevstudios.com>
2021-01-22 21:15:33 -0500

    Add nav wrapper to PostNavigation

--- src/components/molecules/PostNavigation/index.js
+++ src/components/molecules/PostNavigation/index.js
@@ -5,22 +5,24 @@ import styles from './PostNavigation.module.css'
 
 export default function PostNavigation({nextLabel, nextTo, prevLabel, prevTo}) {
   return (
-    <ul className={styles.postNavigation}>
-      <li>
-        {prevTo && (
-          <TextLink to={prevTo} rel="prev">
-            ← {prevLabel || prevTo}
-          </TextLink>
-        )}
-      </li>
-      <li>
-        {nextTo && (
-          <TextLink to={nextTo} rel="next">
-            {nextLabel || nextTo} →
-          </TextLink>
-        )}
-      </li>
-    </ul>
+    <nav className={styles.postNavigation}>
+      <ul>
+        <li>
+          {prevTo && (
+            <TextLink to={prevTo} rel="prev">
+              ← {prevLabel || prevTo}
+            </TextLink>
+          )}
+        </li>
+        <li>
+          {nextTo && (
+            <TextLink to={nextTo} rel="next">
+              {nextLabel || nextTo} →
+            </TextLink>
+          )}
+        </li>
+      </ul>
+    </nav>
   )
 }
 
