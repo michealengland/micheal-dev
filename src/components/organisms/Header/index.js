38a0ad96043010dfbbb46ae419be7f5c4c2a8209
Mike England <mike.england@webdevstudios.com>
2021-01-23 08:34:33 -0500

    Fix Header indentation

--- src/components/organisms/Header/index.js
+++ src/components/organisms/Header/index.js
@@ -4,18 +4,20 @@ import Logo from '@content/assets/me-icon.png'
 import styles from './Header.module.css'
 
 export default function Header() {
-  return <header className={styles.header}>
-    <div className={styles.logo}>
-      <TextLink to={ '/' }>
-        <img src={ Logo } alt="Micheal.dev logo" />
-      </TextLink>
-    </div>
-    <nav className={styles.menu}>
-      <ul>
-        <li>
-          <TextLink to={ '/blog' }>Blog</TextLink>
-        </li>
-      </ul>
-    </nav>
-  </header>
+  return (
+    <header className={styles.header}>
+      <div className={styles.logo}>
+        <TextLink to={ '/' }>
+          <img src={ Logo } alt="Micheal.dev logo" />
+        </TextLink>
+      </div>
+      <nav className={styles.menu}>
+        <ul>
+          <li>
+            <TextLink to={ '/blog' }>Blog</TextLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
 }
