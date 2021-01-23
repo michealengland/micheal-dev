8f525b0d3de58cede5f452fcf9eb6450bcd96e57
Mike England <mike.england@webdevstudios.com>
2021-01-23 14:07:32 -0500

    Remove commented out code

--- src/components/atoms/Logo/index.js
+++ src/components/atoms/Logo/index.js
@@ -1,45 +1,9 @@
 import React from 'react'
-// import styled from '@emotion/styled'
 import Arrow from './ArrowIcon'
 import styles from './Logo.module.css'
 
-// const LogoWrapper = styled.div`
-//   align-items: center;
-//   color: #141826;
-//   background: #f5fffe;
-//   display: flex;
-//   justify-content: center;
-//   position: relative;
-//   height: 100vh;
-//   width: 100vw;
-//   > div {
-//     position: relative;
-//     text-align: center;
-//   }
-//   svg {
-//     fill: currentColor;
-//     position: relative;
-//     width: 35vw;
-//     height: auto;
-//     &:first-child {
-//       animation: rotate-left 1s linear 3s;
-//       animation-fill-mode: forwards;
-//       margin-right: 4vw;
-//       margin-left: -4vw;
-//       transform: rotate(-90deg);
-//     }
-//     &:nth-child(2) {
-//       animation: rotate-right 1s linear 3s;
-//       animation-fill-mode: forwards;
-//       margin-right: -4vw;
-//       margin-left: 4vw;
-//       transform: rotate(90deg);
-//     }
-//   }
-// `
-
-export default function Logo(){
-  return(
+export default function Logo() {
+  return (
     <div className={styles.logo}>
       <div className={styles.inner}>
         <Arrow fill="currentColor" width={200} height={200} />
