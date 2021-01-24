4ae5c2be7ea76b94ecc32d76fc9e09577c52ada4
Mike England <mike.england@webdevstudios.com>
2021-01-23 23:40:27 -0500

    Add propTypes to Error404

--- src/components/organisms/Error404/index.js
+++ src/components/organisms/Error404/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Link} from 'gatsby'
+import PropTypes from 'prop-types'
 
 export default function Error404({content, title}) {
   return (
@@ -10,4 +11,9 @@ export default function Error404({content, title}) {
       <Link to="/">Go home</Link>
     </section>
   )
+}
+
+Error404.propTypes = {
+  title: PropTypes.string.isRequired,
+  content: PropTypes.string
 }
