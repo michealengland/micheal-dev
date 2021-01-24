48797893cb93691c6569a413067636e1a723c7d4
Mike England <mike.england@webdevstudios.com>
2021-01-23 23:10:45 -0500

    Remove extra wrapper on Logo

--- src/components/atoms/Logo/index.js
+++ src/components/atoms/Logo/index.js
@@ -4,11 +4,9 @@ import styles from './Logo.module.css'
 
 export default function Logo() {
   return (
-    <div className={styles.logo}>
-      <div className={styles.inner}>
-        <Arrow fill="currentColor" width={200} height={200} />
-        <Arrow fill="currentColor" width={200} height={200} />
-      </div>
-    </div>
+    <span className={styles.logo}>
+      <Arrow fill="currentColor" width={200} height={200} />
+      <Arrow fill="currentColor" width={200} height={200} />
+    </span>
   )
 }
