8c991bca854abf7d219bf435b45ca78b8b2dc1b2
Mike England <mike.england@webdevstudios.com>
2021-01-23 23:43:00 -0500

    Add styles to Error404 component

--- src/components/organisms/Error404/index.js
+++ src/components/organisms/Error404/index.js
@@ -1,13 +1,14 @@
 import React from 'react'
-import {Link} from 'gatsby'
 import PropTypes from 'prop-types'
+import {Link} from 'gatsby'
+import styles from './Error404.module.css'
 
 export default function Error404({content, title}) {
   return (
-    <section>
+    <section className={styles.error404}>
       <title>{title}</title>
-      <h1>{title}</h1>
-      {!!content && <p>{content}</p>}
+      <h1 className={styles.title}>{title}</h1>
+      {!!content && <p className={styles.content}>{content}</p>}
       <Link to="/">Go home</Link>
     </section>
   )
