041b41155fc34add5af381b80877b49430d8a982
Mike England <mike.england@webdevstudios.com>
2021-01-23 23:39:00 -0500

    Update Error404 with props

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,30 +1,15 @@
 import * as React from 'react'
-import {Link} from 'gatsby'
 import Layout from '@components/organisms/Layout'
+import Error404 from '@components/organisms/Error404'
 
 // markup
 const NotFoundPage = () => {
   return (
     <Layout>
-      <title>Not found</title>
-      <h1>Page not found</h1>
-      <p>
-        Sorry{' '}
-        <span role="img" aria-label="Pensive emoji">
-          😔
-        </span>{' '}
-        we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === 'development' ? (
-          <>
-            <br />
-            Try creating a page in <code>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
+      <Error404
+        title="404 Page not found."
+        content="Sorry, the content you're looking for is missing."
+      />
     </Layout>
   )
 }
