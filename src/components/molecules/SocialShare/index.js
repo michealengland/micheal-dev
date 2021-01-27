d0134ad214dcb4a6249f8129eeebf0483acf4401
Mike England <mike.england@webdevstudios.com>
2021-01-27 00:01:10 -0500

    Setup social account links

--- src/components/molecules/SocialShare/index.js
+++ src/components/molecules/SocialShare/index.js
@@ -1,11 +1,37 @@
 import React from 'react'
 
 export default function SocialShare() {
+  const socialAccounts = [
+    {
+      icon: '',
+      label: 'Github',
+      url: 'https://github.com/michealengland'
+    },
+    {
+      icon: '',
+      label: 'Linkedin',
+      url: 'https://www.linkedin.com/in/michealengland/'
+    },
+    {
+      icon: '',
+      label: 'Twitter',
+      url: 'https://twitter.com/mikelikethebike'
+    },
+  ]
+
   return (
-    <ul>
-      <li>Github</li>
-      <li>Twitter</li>
-      <li>LinkedIn</li>
-    </ul>
+    <nav role="navigation" aria-label="social links">
+      <ul>
+        { socialAccounts.map((account) => {
+          const {icon, label, url} = account
+
+          return (
+            <li key={`social-${icon}`}>
+              <a href={url}><span>{icon}</span>{label}</a>
+            </li>
+          )
+        }) }
+      </ul>
+    </nav>
   )
 }
