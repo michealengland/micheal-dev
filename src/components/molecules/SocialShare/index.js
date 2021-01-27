3cfbe1b677f706875c297a414c1a89ef16f4411d
Mike England <mike.england@webdevstudios.com>
2021-01-26 23:46:53 -0500

    Setup SocialShare component

---
+++ src/components/molecules/SocialShare/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function SocialShare() {
+  return (
+    <ul>
+      <li>Github</li>
+      <li>Twitter</li>
+      <li>LinkedIn</li>
+    </ul>
+  )
+}
