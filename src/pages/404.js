5c1b221886e853d5af8b40995f472fb87bc07f8f
Mike England <mike.england@webdevstudios.com>
2021-01-27 08:59:37 -0500

    Update 404 content

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,13 +2,12 @@ import * as React from 'react'
 import Layout from '@components/organisms/Layout'
 import Error404 from '@components/organisms/Error404'
 
-// markup
 const NotFoundPage = () => {
   return (
     <Layout>
       <Error404
-        title="404 Page not found."
-        content="Sorry, the content you're looking for is missing."
+        title="404 page not found"
+        content="This page is missing or does not exist."
       />
     </Layout>
   )
