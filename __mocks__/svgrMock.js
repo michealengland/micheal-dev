8fda87059c6196c18a4236b0ee70716d43cb531b
Mike England <mike.england@webdevstudios.com>
2021-01-27 23:12:33 -0500

    Setup custom mock for svg

--- __mocks__/svgrMock.js
+++ __mocks__/svgrMock.js
@@ -1,4 +1,17 @@
-import * as React from 'react'
+// import * as React from 'react'
+import React from 'react'
 
 export default 'SvgrURL'
-export const ReactComponent = 'div'
+
+export function ReactComponent() {
+  return (
+    <svg
+	  className="svg-mock"
+      height={200}
+      viewBox="0 0 32 32"
+      width={200}
+    >
+      <path d="M32 32 32 27.5992141 5.00813008 15.9685658 32 4.27504912 32 0 0 13.956778 0 17.9803536 15.8090702 24.9065278z" />
+    </svg>
+  )
+}
