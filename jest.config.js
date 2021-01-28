53b37fc7e2b990b57d2e99b50092623415454e33
Mike England <mike.england@webdevstudios.com>
2021-01-27 22:45:05 -0500

    Add mock svg settings to Jest config
    Future selft notes: The `.svg` file type from `file-mock.js` was causing conflicts
    with the svg mock setup. Removing the extension fixed the issue.
    
    Make sure to check for existing file types before adding new mocks.

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,8 @@ module.exports = {
   },
   moduleNameMapper: {
     '.+\\.(css|styl|less|sass|scss)$': 'identity-obj-proxy',
-    '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/file-mock.js',
+    '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/file-mock.js',
+    '\\.svg$': '<rootDir>/__mocks__/svgrMock.js',
     '^@components(.*)$': '<rootDir>/src/components$1',
     '^@utilities(.*)$': '<rootDir>/src/utilities$1',
     '^@static(.*)$': '<rootDir>/src/static$1',
