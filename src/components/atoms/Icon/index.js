73ca739bdc4c8c755c0c2c7fb6105adc6a3ab7a5
Mike England <mike.england@webdevstudios.com>
2021-01-27 22:23:51 -0500

    Fix icon svg imports
    Setup direct imports from SVG files using SVGR.

--- src/components/atoms/Icon/index.js
+++ src/components/atoms/Icon/index.js
@@ -1,16 +1,31 @@
 import PropTypes from 'prop-types'
 import React from 'react'
-import {ReactComponent as GithubLogo} from './svgs/github.svg'
-import {ReactComponent as TwitterLogo} from './svgs/twitter.svg'
-import {ReactComponent as LinkedinLogo} from './svgs/linkedin.svg'
-const icons = {
-  github: <GithubLogo />,
-  linkedin: <LinkedinLogo />,
-  twitter: <TwitterLogo />,
-}
+import Github from './svgs/github.svg'
+import Twitter from './svgs/twitter.svg'
+import Linkedin from './svgs/linkedin.svg'
+
 export default function Icon({ariaHidden, className, icon}) {
+  let element = null
+
+  switch (icon) {
+  case 'github':
+    element = <Github />
+    break
+
+  case 'linkedin':
+    element = <Linkedin />
+    break
+
+  case 'twitter':
+    element = <Twitter />
+    break
+
+  default:
+    break
+  }
+
   return (
-    <span className={className} aria-hidden={ariaHidden}>{ icons[icon] }</span>
+    <span className={className} aria-hidden={ariaHidden}>{ element }</span>
   )
 }
 
