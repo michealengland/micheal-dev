fcc3f576ae1b9c7bc42f5fb3453788016f5f3247
Mike England <mike.england@webdevstudios.com>
2021-01-27 23:59:25 -0500

    Rename SocialShare snapshot to SocialLinks

---
+++ src/__tests__/SocialLinks.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+import SocialLinks from '@components/molecules/SocialLinks'
+describe('SocialLinks', () => {
+  it('renders correctly', () => {
+    const tree = renderer
+      .create(
+        <SocialLinks />
+      )
+      .toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+})
+// RENAME THIS FILE
