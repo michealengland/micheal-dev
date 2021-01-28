f6aedadb6b8a4d59867bb6b99f0e4ab0d72291f9
Mike England <mike.england@webdevstudios.com>
2021-01-27 22:25:21 -0500

    Update test snapshots for new test

---
+++ src/__tests__/SocialShare.js
@@ -0,0 +1,13 @@
+import React, {Fragment} from 'react'
+import renderer from 'react-test-renderer'
+import SocialShare from '@components/molecules/SocialShare'
+describe('SocialShare', () => {
+  it('renders correctly', () => {
+    const tree = renderer
+      .create(
+        <SocialShare />
+      )
+      .toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+})
