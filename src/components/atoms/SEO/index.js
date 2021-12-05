3f8a29843607ce56aaec7b3eaffff6aa98f30eb7
Mike England <mike.england@webdevstudios.com>
2021-12-05 13:19:55 -0500

    Add Twitter image to SEO component

--- src/components/atoms/SEO/index.js
+++ src/components/atoms/SEO/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import {StaticQuery, graphql} from 'gatsby'
+import Avatar from '@content/assets/me-avatar.jpg'
 
 function SEO({description, lang, meta, keywords, title}) {
   return (
@@ -50,6 +51,10 @@ function SEO({description, lang, meta, keywords, title}) {
                 name: 'twitter:description',
                 content: metaDescription,
               },
+              {
+                name: 'twitter:image',
+                content: Avatar
+              }
             ]
               .concat(
                 keywords.length > 0
