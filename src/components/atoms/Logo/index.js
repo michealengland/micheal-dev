9c7b91ebba0048942f4064783c864e18f2013249
Mike England <micheallengland@gmail.com>
2023-10-01 18:21:42 -0400

    revert logo file styles
    Originally, the file name being named as Logo.module.css was causing issues.
    This change is no longer needed with other fixes that have been applied.

--- src/components/atoms/Logo/index.js
+++ src/components/atoms/Logo/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import styles from './Logo.module.css'
+import styles from './logo.css'
 import {ReactComponent as ArrowIcon} from './arrow.svg'
 
 export default function Logo() {
