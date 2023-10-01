d611c102bc8edb58ee6eba36acc1e2fbc7bbca08
Mike England <micheallengland@gmail.com>
2023-10-01 18:20:26 -0400

    Update main layout with global styles

--- nextjs-migration/micheal-dev/src/app/layout.tsx
+++ nextjs-migration/micheal-dev/src/app/layout.tsx
@@ -1,4 +1,8 @@
-import './globals.css'
+import '@fontsource/mulish/300.css'
+import '@fontsource/mulish/400.css'
+import '@fontsource/mulish/600.css'
+import '@fontsource/mulish/700.css'
+import './styles.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
