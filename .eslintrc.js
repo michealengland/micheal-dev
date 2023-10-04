aa56d11e561d506b8d00125c64cb6d1f9b48c18d
Mike England <mikeengland@kinsta.com>
2023-10-03 23:06:12 -0400

    fix missing eslint dependency

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,51 +1,52 @@
 module.exports = {
-    'env': {
-        'browser': true,
-        'es6': true
+  'env': {
+    'browser': true,
+    'es6': true
+  },
+  'extends': [
+    'react-app',
+    // 'eslint:recommended',
+    'next/core-web-vitals',
+    'plugin:jsx-a11y/recommended',
+    'plugin:react/recommended'
+  ],
+  'globals': {
+    'Atomics': 'readonly',
+    'SharedArrayBuffer': 'readonly'
+  },
+  'parserOptions': {
+    'ecmaFeatures': {
+      'jsx': true
     },
-    'extends': [
-        'react-app',
-        // 'eslint:recommended',
-        'plugin:jsx-a11y/recommended',
-        'plugin:react/recommended'
+    'ecmaVersion': 2020,
+    'sourceType': 'module'
+  },
+  'plugins': [
+    'react',
+    'jsx-a11y'
+  ],
+  'rules': {
+    'indent': [
+      'error',
+      2
     ],
-    'globals': {
-        'Atomics': 'readonly',
-        'SharedArrayBuffer': 'readonly'
-    },
-    'parserOptions': {
-        'ecmaFeatures': {
-            'jsx': true
-        },
-        'ecmaVersion': 2020,
-        'sourceType': 'module'
-    },
-    'plugins': [
-        'react',
-        'jsx-a11y'
+    'linebreak-style': [
+      'error',
+      'unix'
     ],
-    'rules': {
-        'indent': [
-            'error',
-            2
-        ],
-        'linebreak-style': [
-            'error',
-            'unix'
-        ],
-        'quotes': [
-            'error',
-            'single'
-        ],
-        'semi': [
-            'error',
-            'never'
-        ],
-        'object-curly-spacing': [
-          'error', 'never'
-        ],
-        'space-in-parens': [
-          'error', 'never'
-        ]
-    }
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'never'
+    ],
+    'object-curly-spacing': [
+      'error', 'never'
+    ],
+    'space-in-parens': [
+      'error', 'never'
+    ]
+  }
 }
