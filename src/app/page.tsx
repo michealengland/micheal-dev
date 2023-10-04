cf9e420ad387d375a8edc2d6c56c8e5a9e8cc3a0
Mike England <mikeengland@kinsta.com>
2023-10-03 22:04:02 -0400

    major updates to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,57 +4,53 @@ import Hero from '@components/molecules/Hero'
 import Content from '@components/molecules/Content'
 import MediaContent from '@components/molecules/MediaContent'
 import ContentCTA from '@components/molecules/ContentCTA'
-import HeroBackgroundImage from '@assets/adrien-converse-REL986jyuX4-unsplash.jpg'
+import HeroBackgroundImage from '@assets/prague-cityscape-2000x1000.jpg'
 import ContentImage1 from '@assets/anas-alshanti-feXpdV001o4-unsplash.jpg'
 import ContentImage2 from '@assets/harpal-singh-_zKxPsGOGKg-unsplash.jpg'
-import ContentImage3 from '@assets/github-contributions-edited.jpg'
-import ContentImage4 from '@assets/adrien-converse-kCrrUx7US04-unsplash.jpg'
+import ContentImage3 from '@assets/350zduino-1080x720.jpg'
+import ContentImage4 from '@assets/prague-library-2000x1000.jpg'
 
 export default function TestPage() {
   return (
     <Layout>
       <Hero
-        title="I'm Mike England"
-        content="JavaScript Developer / Motorcycle Enthusiast"
+        title="Mike England"
+        content="Development Team Lead @Kinsta"
         img={HeroBackgroundImage.src}
       />
       <Content
         title="About Me"
         titleTag="h2"
-        content={'I\'m a JavaScript Developer at <a href="https://kinsta.com/">Kinsta</a> and I\'m passionate about building awesome websites, tools, and web applications using JavaScript.'}
+        content={'I\'m a Development Team Lead at <a href="https://kinsta.com/">Kinsta</a> and I\'m passionate about building awesome products, tools, and web applications.'}
       />
       <MediaContent
         imgClipRight={true}
         img={ContentImage1.src}
-        title="Code Skills"
+        title="Career Skills"
         content={`
-        <p>Here are some of the areas I'm focused on currently:</p>
+        <p>In April of 2022, I transitioned to a development team lead role which shifted my focus away from React, JavaScript, Node, and Jest. Don't worry, I still love doing personal projects and helping out! Now, I'm more focused on leading and managing a team.</p>
         <ul>
-          <li>Gatsby</li>
-          <li>Gutenberg</li>
-          <li>JavaScript</li>
-          <li>Jest</li>
-          <li>Next.js</li>
-          <li>Node</li>
-          <li>React</li>
-          <li>TypeScript</li>
-          <li>Webpack</li>
+          <li>Performance Reviews & 1:1s</li>
+          <li>Mentoring</li>
+          <li>Product Ownership</li>
+          <li>Team Management & Growth</li>
+          <li>Coordinatin
         </ul>
         `}
       />
       <MediaContent
         img={ContentImage2.src}
         title="Design Background"
-        content="<p>Like many front-enders I started out as a designer, creating everything from branding projects to website concepts. To this day, I apply many designer disciplines into creating, building, and testing applications.</p>"
+        content="<p>I believe it's important to remember where you started and my journey started out designer. I spent years in the field creating everything from branding projects to website concepts. To this day, I apply many designer disciplines into creating, building, and testing applications.</p>"
       />
       <MediaContent
         imgClipRight={true}
         img={ContentImage3.src}
-        title="#100DaysOfCode"
-        content="<p>Whether you're a seasoned engineer or new to development, #100DaysOfCode is a fun and exciting way to level up. Currently, I'm on my second round, and I'm focused on making React applications, unit testing with Jest, and diving deeper in advanced JavaScript techniques.</p>"
+        title="Arduino & Drifting"
+        content="<p>In my free time, I try to combine my love of tech and drifting to do some fun projects on my 350z!</p>"
       />
       <ContentCTA
-        content={'Being an engineer is awesome! Solving code challenges and building out the "What if..." ideas are what I the enjoy most. In my free time, I love to share open source projects that I\'m working on.'}
+        content={'I enjoy solving challenges and creating products with code. In fact, this entire website along with many of my other personal projects are on GitHub.'}
         label="View My GitHub Profile"
         title="Checkout some of my work"
         url="https://github.com/michealengland?tab=repositories"
