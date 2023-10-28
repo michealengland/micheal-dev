26cb80b7ced4d2d21b1ec9c9a2f76860219b859d
Mike England <mikeengland@kinsta.com>
2023-10-28 11:45:29 -0400

    TEMP: Replace NextImage with img tag
    Testing this due to what I believe are issues with the way the image is
    utilized within Next on generated pages.

--- src/components/molecules/AuthorBio/index.js
+++ src/components/molecules/AuthorBio/index.js
@@ -2,18 +2,17 @@
 import React from 'react'
 import Avatar from '@assets/mike-in-prague-2-500x500.jpg'
 import styles from './AuthorBio.module.css'
-import Image from 'next/image'
+// import Image from 'next/image'
 
 export default function AuthorBio() {
   return (
     <div className={styles.authorBio}>
-      <Image
+      <img
         className={styles.avatar}
-        src={Avatar?.src} // Testing to see if this fixes an issue.
+        src={Avatar.src}
         alt="Micheal England"
         width={100}
         height={100}
-        unoptimized={false} // Testing to see if this fixes an issue.
       />
       <p className={styles.content}>
         Article by <strong>Micheal England</strong>.
